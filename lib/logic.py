c8da960f9e2cb8accf5efb0c8703eaa369f727f6
Abdullah Makhdoom @ Work <abdullahmakhdoom1998@gmail.com>
2022-11-05 22:01:58 +0500

    adding search_wiki()

--- lib/logic.py
+++ lib/logic.py
@@ -5,3 +5,8 @@ def wiki(name="Cristiano Ronaldo", length=1):
     """This is a wikipedia fetcher"""
 
     return wikipedia.summary(name, length)
+
+def search_wiki(name : str):
+    
+    results = wikipedia.search(name)
+    return results
